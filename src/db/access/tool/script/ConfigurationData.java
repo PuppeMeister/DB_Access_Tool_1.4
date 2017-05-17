7b5b5d36c4cd4a515152df5a331aef7ff6d24e12
PuppeMeister <puppe.meister@gmail.com>
2017-05-17 15:13:24 +0700

    2nd Commit - Adding ConfigurationData Class This class is intended to keep configuration data, replacing HashMap or other array object.

---
+++ src/db/access/tool/script/ConfigurationData.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package db.access.tool.script;
+
+/**
+ *
+ * @author rinnadia
+ */
+public class ConfigurationData {
+    
+    private String[] listServer;
+    private String[] listUser;
+    private String ip;
+    private String port;
+    private String user;
+    private String password;
+    private String DB_ADMIN_IP;
+    private String DB_ADMIN_PORT;
+    private String DB_ADMIN_USER;
+    private String DB_ADMIN_PASS;
+    private String DB_ADMIN_DATABASE;
+    private String DB_ADMIN_TABLE;
+    private String[] LIST_ADMIN;
+    
+}
